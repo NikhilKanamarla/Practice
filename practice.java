63887a80208977bb9e178c85dff06ba8eca4112c
NKdeveloper <nkanama@umich.edu>
2019-08-12 12:19:15 -0400

    Some light programming
    Working on some java

--- practice.java
+++ practice.java
@@ -1,3 +1,4 @@
+import java.io.*;
 // class name must match file name
 public class practice {
     //class variables
@@ -23,6 +24,7 @@ public void setSuccess (boolean mission) {
 public boolean getSuccess () {
     return missionSucess;
 }
+
 
 // main method must be in file name class
 public static void main(String[] args) {
@@ -52,6 +54,26 @@ public static void main(String[] args) {
     //returned variable values need to be stored
     boolean missonControl = Apollo.getSuccess();
     System.out.println(missonControl);
+    practice artemis = new practice((2024), 10000, "SLS");
+    artemis.setSuccess(false);
+    boolean trumpNasa = artemis.getSuccess();
+    System.out.println(trumpNasa); 
+
+    // variables are pass by value
+    String valueSent = "MIT";
+    artemis.modifyByValue(valueSent);
+    System.out.println(valueSent);
+    // Use return statement to get value from method 
+    String returnedValue = artemis.modifyByValueAndReturn(valueSent);
+    System.out.println(returnedValue);
+
+    //method overloading practice
+    int overload = 5;
+    double overload2 = 10.0;
+    int storeOverload1 = Apollo.methodOverload(overload);
+    double storeOverload2 = Apollo.methodOverload(overload2);
+    System.out.println("with an int var " + storeOverload1 + " with a double var " + storeOverload2);
+
 }
 
 //method needs variable type and return statment in this case
@@ -64,6 +86,30 @@ public static String[][] printAndModify(String array [] []) {
     array[1][1] = "Michigan Hackathon Alliance";
 
     return array;
+}
+
+//pass-by-value meants that the orginal value of the variable sent in cannot be changed, only copied
+public void modifyByValue(String word) {
+    word += " I have been to the method";
+    System.out.println(word);
+}
+
+// variable can be different value if returned 
+public String modifyByValueAndReturn(String word) {
+    word += " I have been to the method mark 2";
+    System.out.println(word);
+    return word;
+}
+
+// method overloading is when two methods have the same name but different method variables
+public int methodOverload(int value) {
+    value*=2;
+    return value;
+}
+
+public double methodOverload(double value) {
+        value *= 2;
+        return value;
 }
 
 }
