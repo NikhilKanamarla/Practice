66a4e3847fc73597a7d1b1f76dcece830b038737
NKdeveloper <nkanama@umich.edu>
2019-08-25 20:26:40 -0400

    Reviewing matlab code
    A script, a function

---
+++ MATLAB/hello2Function.m
@@ -0,0 +1,10 @@
+function [x,y] = hello2Function(num1,num2)
+%hello2n is the name of function
+%x and y are outputs 
+%name of method must be name of file
+num1 = num1 * 2;
+num2 = num2 * 2;
+x = num1;
+y = num2;
+end
+
