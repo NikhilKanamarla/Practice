905659a61a283d9f01f87bb13f479454a7e2b71a
NKdeveloper <nkanama@umich.edu>
2019-08-27 12:39:07 -0400

    Quick review
    just some syntax

---
+++ PracticePlayground.playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+// just some basic review
+var str = "Hello, playground"
+print (str)
+var hello = "Hello World"
+print (hello)
+var age = 18
+if(age == 18) {
+    print("my age is ", age)
+} else {
+    print("your still a minor")
+}
+let name = "Nikhil"
+var num = 5*2/3%5
+print(num)
+while (num < 5) {
+    num = num + 1
+    print("Still going")
+}
+var buildingNames: [String] = ["EECS Building", "Bob Byester Building", "Mason Hall"]
+
+for index in buildingNames {
+    print (index)
+}
+
+
+
